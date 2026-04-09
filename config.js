737937d83ede444e0253db7575c3372a395538d8
Jorge <jespindolacastillo@github.com>
2026-04-08 21:29:32 -0600

    Identity sync: Fixed logo text colors

--- config.js
+++
@@ -1,7 +0,0 @@
-window.tenantConfig = {
-    name: 'SaaS Platform',
-    logoUrl: 'logo.png',
-    primaryColor: '#2563eb',
-    supabaseUrl: 'https://qdbosheknbgyqhtoxmfv.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkYm9zaGVrbmJneXFodG94bWZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzMzU4MzAsImV4cCI6MjA4ODkxMTgzMH0.x1QtKn5dXj30gH7e-w31OrkrSBfIQS9hr2Yiq9Rlxik'
-};
